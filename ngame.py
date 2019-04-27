1439b66acb501bbd9c79fc480cf0651b69727458
jcraver1021 <elciego@gmail.com>
2019-04-27 17:08:50 -0400

    Allow players to learn multiple games and positions

--- ngame.py
+++ ngame.py
@@ -31,61 +31,81 @@ class BuildMode(Enum):
     RANDOM = 2
 
 
+_build_method = {
+    BuildMode.PURE: lambda n, i: np.array([1 if j == i else 0 for j in range(n)]),
+    BuildMode.UNIFORM: lambda n: _normalize(np.ones(n)),
+    BuildMode.RANDOM: lambda n: _normalize(np.random.random(n))
+}  # type: Dict[BuildMode, Callable]
+
+
+def build_strategy(n, mode, **kwargs):
+    # type: (int, BuildMode, Optional[Dict[Any, Any]]) -> np.array
+    """
+    Build a player's strategy vector
+
+    Args:
+        n: The size of the vector
+        mode: Which mode to use when building the vector
+        kwargs: Arguments for specific build modes
+            PURE: requires i (int), which is the active strategy
+    """
+    return _build_method[mode](n, **kwargs)
+
+
 class Player:
-    def __init__(self, strategies):
+    def __init__(self):
         # type: (np.array) -> None
         """
-        Initialize a player with a set of strategies, 0 winnings, and an empty history.
+        Initialize a player with 0 winnings and an empty history.
+        """
+
+        # State maintained on player level
+        self.winnings = 0.0  # type: float
+
+        # State maintained on game level
+        self.strategies = {}  # type: Dict[NPlayerGame, Dict[int, np.array]]
+        self.history = {}  # type: Dict[NPlayerGame, Dict[int, List[int]]]
+
+    def learn(self, game, i, strategy):
+        # type: (NPlayerGame, int, np.array) -> None
+        """
+        Learn (or relearn) a strategy for a game
 
         Args:
-            strategies: A numpy array representing the probability that this player will use
+            game: The NPlayerGame this strategy applies to
+            i: Which player you are in this setting
+            strategy: A numpy array representing the probability that this player will use
                 each strategy at their disposal
         Raises:
             ValueError if the vector is not 1-dimensional or the l-1 magnitude of the
                 strategy vector is not close to 1
         """
-        if len(strategies.shape) != 1:
-            raise ValueError('Strategy should be a vector but has shape {}'.format(strategies.shape))
-        magnitude = np.linalg.norm(strategies, ord=1)
+        if len(strategy.shape) != 1:
+            raise ValueError('Strategy should be a vector but has shape {}'.format(strategy.shape))
+        magnitude = np.linalg.norm(strategy, ord=1)
         if not np.isclose(magnitude, 1):
             raise ValueError('Strategy vector is magnitude {}, not 1'.format(magnitude))
 
-        self.strategies = strategies  # type: np.array
+        if game not in self.strategies:
+            self.strategies[game] = {}
+            self.history[game] = {}
+        self.strategies[game][i] = strategy
+        self.history[game][i] = []
 
-        # Accounts maintained on player level
-        self.winnings = 0.0  # type: float
-        self.history = []  # type: List[int]
-
-    # Maps build mode to method
-    _build_method = {
-        BuildMode.PURE: lambda n, i: np.array([1 if j == i else 0 for j in range(n)]),
-        BuildMode.UNIFORM: lambda n: _normalize(np.ones(n)),
-        BuildMode.RANDOM: lambda n: _normalize(np.random.random(n))
-    }  # type: Dict[BuildMode, Callable]
-
-    @classmethod
-    def build_player(cls, n, mode, **kwargs):
-        # type: (int, BuildMode, Optional[Dict[Any, Any]]) -> Player
+    def play(self, game, i):
+        # type: (NPlayerGame, int) -> int
         """
-        Build a player using the specified build mode
-
-        Args:
-            n: number of strategies the player has
-            mode: the build mode to use
-            kwargs: Optional arguments
-                PURE: requires i (int)
-        """
-        return Player(Player._build_method[mode](n, **kwargs))
-
-    def play(self):
-        # type: () -> int
-        """
-        Choose a strategy based on the stored probabilities.
+        Choose a strategy based on the stored probabilities for that game.
 
         Choice of strategy is appended to personal history.
+
+        Args:
+            game: The NPlayerGame instance to play
+            i: Which player you are in this setting
         """
-        self.history.append(np.random.choice(len(self.strategies), p=self.strategies))
-        return self.history[-1]
+        strategies = self.strategies[game][i]
+        self.history[game][i].append(np.random.choice(len(strategies), p=strategies))
+        return self.history[game][i][-1]
 
     def pay(self, winnings):
         # type: (float) -> None
@@ -99,7 +119,7 @@ class Player:
 
 
 class NPlayerGame:
-    def __init__(self, payoff, players):
+    def __init__(self, payoff):
         """
         An N-Player game based on a set n+1-dimensional array.
         The number of players and the number of strategies each player has are interpreted solely
@@ -120,34 +140,36 @@ class NPlayerGame:
                 'Shape of strategy product is {}; final dimension should be {}, not {}'.format(
                     payoff.shape[:-1], n, payoff.shape[-1]))
 
-        if len(players) != n:
-            raise ValueError(
-                'The game is for {} players, but the number of players is {}'.format(n, len(players)))
-
         self.payoff = payoff  # type: np.array
         self.n = n  # type: int
-        self.players = players  # type: List[Player]
 
-    def run(self, n=1):
-        # type: (Optional[int]) -> None
+    def run(self, players, n=1):
+        # type: (List[Player], Optional[int]) -> None
         """
         Run the game n times
 
         Args:
+            players: Players playing the game
             n: times to run the game
         """
-        for _ in range(n):
-            plays = tuple(player.play() for player in self.players)
-            for player, winnings in zip(self.players, self.payoff[plays]):
+        if len(players) != self.n:
+            raise ValueError(
+                'The game is for {} players, but the number of players is {}'.format(self.n, len(players)))
+
+        for i in range(n):
+            plays = tuple(player.play(self, j) for j, player in enumerate(players))
+            for player, winnings in zip(players, self.payoff[plays]):
                 player.pay(winnings)
 
 
 if __name__ == '__main__':
     A = np.random.random((5, 6, 7, 3)) - 0.5
-    P = [Player.build_player(3, BuildMode.RANDOM) for _ in range(A.shape[-1])]
-    game = NPlayerGame(A, P)
+    game = NPlayerGame(A)
+    P = [Player() for _ in range(A.shape[-1])]
+    for i, p in enumerate(P):
+        p.learn(game, i, build_strategy(A.shape[i], BuildMode.RANDOM))
     print('Game:\n{}'.format(game.payoff))
-    game.run(20)
+    game.run(P, 20)
     for i, p in enumerate(P):
         print('Player {} has {}'.format(i, p.winnings))
         print('Player {} played {}'.format(i, p.history))
