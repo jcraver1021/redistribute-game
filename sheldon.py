2c0a56d22253e22b47615082273175ae327f6959
jcraver1021 <elciego@gmail.com>
2019-05-06 21:59:50 -0400

    Applied rotation-based strategies to Sheldon game

--- sheldon.py
+++ sheldon.py
@@ -1,16 +1,35 @@
 from ngame import *
-
-
-def try_game(game, players, strategies, n=1):
-    """Teach players a game and then run it"""
-    for i in range(len(players)):
-        players[i].learn(game, i, strategies[i])
-    game.run(players, n)
+from tester import rotate_strategies
 
 
 def winnings_total(players):
+    # type: (List[Player]) -> str
     """Sum the winnings of all players"""
     return 'Total: ${}'.format(sum(map(lambda p: p.winnings, players)))
+
+
+def print_players(players):
+    # type: (List[Player]) -> None
+    """Print the winnings of the players"""
+    for player in players:
+        print(player)
+    print(winnings_total(players))
+    print()
+
+
+# This is our mechanism for this game; punish payers who repeat their strategies
+def repeat_mechanism(payoff, players, game, penalty=lambda x: x):
+    # type: (np.array, List[Player], NPlayerGame, Optional[Callable]) -> np.array
+    """Punish players for repeating their strategy"""
+    x = np.copy(payoff)
+    for i, player in enumerate(players):
+        history = player.history[game][i]
+        n = len(history)
+        j = n - 2
+        while n > 0 and j >= 0 and history[-1] == 0 and history[-1] == history[j]:
+            j -= 1
+        x[i] -= penalty(n - j - 2)
+    return x
 
 
 names = ['Sheldon', 'Raj', 'Leonard']
@@ -26,106 +45,112 @@ game_payoff[0, 1, 1] = [1, 4, 4]
 game_payoff[1, 1, 1] = [6, -4, -4]
 game = NPlayerGame(game_payoff)
 
-print('Random Sheldon')
-mixed = build_strategy(2, BuildMode.UNIFORM)
+strategy_home = build_strategy(2, BuildMode.PURE, i=0)
+strategy_away = build_strategy(2, BuildMode.PURE, i=1)
+strategy_mixed = build_strategy(2, BuildMode.UNIFORM)
+
+print("Mixed: No Mechanism")
 players = Player.make_n_players(3, names)
-for i, player in enumerate(players):
-    player.learn(game, i, mixed)
-game.run(players, 1000)
-for player in players:
-    print(player)
-print(winnings_total(players))
-print()
+rotate_strategies(game, players, [[strategy_mixed]] * 3, 1000)
+print_players(players)
 
-print('Stay at home')
-stayhome = build_strategy(2, BuildMode.PURE, i=0)
+print("Mixed: Repeat Mechanism")
 players = Player.make_n_players(3, names)
-for i, player in enumerate(players):
-    player.learn(game, i, stayhome)
-game.run(players, 1000)
-for player in players:
-    print(player)
-print(winnings_total(players))
-print()
+rotate_strategies(game, players, [[strategy_mixed]] * 3, 1000, mechanism=repeat_mechanism)
+print_players(players)
 
-print('What happens if Sheldon stays home?')
-party = build_strategy(2, BuildMode.PURE, i=1)
+print("Staying Home: No Mechanism")
 players = Player.make_n_players(3, names)
-for i, player in enumerate(players):
-    player.learn(game, i, stayhome if i == 0 else party)
-game.run(players, 1000)
-for player in players:
-    print(player)
-print(winnings_total(players))
-print()
+rotate_strategies(game, players, [[strategy_home]] * 3, 1000)
+print_players(players)
 
-print('What happens if everyone else stays home?')
+print("Staying Home: Repeat Mechanism")
 players = Player.make_n_players(3, names)
-for i, player in enumerate(players):
-    player.learn(game, i, stayhome if i != 0 else party)
-game.run(players, 1000)
-for player in players:
-    print(player)
-print(winnings_total(players))
-print()
+rotate_strategies(game, players, [[strategy_home]] * 3, 1000, mechanism=repeat_mechanism)
+print_players(players)
 
+rotation = [
+    [
+        strategy_away,
+    ],
+    [
+        strategy_home,
+        strategy_away
+    ],
+    [
+        strategy_away,
+        strategy_home
+    ]
+]
 
-# This is our mechanism for this game; punish payers who repeat their strategies
-def repeat_mechanism(payoff, players, game, penalty=lambda x: x):
-    # type: (np.array, List[Player], NPlayerGame) -> np.array
-    """Punish players for repeating their strategy"""
-    x = np.copy(payoff)
-    for i, player in enumerate(players):
-        history = player.history[game][i]
-        n = len(history)
-        j = n - 2
-        while n > 0 and j >= 0 and history[-1] == history[j]:
-            j -= 1
-        x[i] -= penalty(n - j - 2)
-    return x
-
-
-# Now try random
-print('Random Sheldon')
+print("Two-Day Rotation: No Mechanism")
 players = Player.make_n_players(3, names)
-for i, player in enumerate(players):
-    player.learn(game, i, mixed)
-game.run(players, 1000, mechanism=repeat_mechanism)
-for player in players:
-    print(player)
-print(winnings_total(players))
-print()
+rotate_strategies(game, players, rotation, 1000)
+print_players(players)
 
-# And party
-print('What happens if Sheldon stays home?')
+print("Two-Day Rotation: Repeat Mechanism")
 players = Player.make_n_players(3, names)
-for i, player in enumerate(players):
-    player.learn(game, i, stayhome if i == 0 else party)
-game.run(players, 1000, mechanism=repeat_mechanism)
-for player in players:
-    print(player)
-print(winnings_total(players))
-print()
+rotate_strategies(game, players, rotation, 1000, mechanism=repeat_mechanism)
+print_players(players)
 
-# The strategy now is to do whichever option hurts less
-print('Try to deal with the mechanism')
+rotation = [
+    [
+        strategy_home,
+        strategy_away,
+    ],
+    [
+        strategy_away,
+        strategy_home,
+        strategy_away,
+        strategy_away
+    ],
+    [
+        strategy_away,
+        strategy_home,
+        strategy_away,
+        strategy_away
+    ]
+]
+
+print("Four-Day Rotation: No Mechanism")
 players = Player.make_n_players(3, names)
-for i in range(1000):
-    for j, player in enumerate(players):
-        player.learn(game, j, stayhome)
-        if i > 0:
-            player.history[game][j].append(0)
-            if j == 0:
-                worst_case = -4
-            else:
-                worst_case = -1
-            potential = repeat_mechanism(np.ones(3), players, game)[j]
-            if potential < worst_case:
-                player.learn(game, j, party)
-            elif potential == worst_case:
-                player.learn(game, j, mixed)
-            player.history[game][j].pop()
-    game.run(players, mechanism=repeat_mechanism)
-for player in players:
-    print(player)
-print(winnings_total(players))
+rotate_strategies(game, players, rotation, 1000)
+print_players(players)
+
+print("Four-Day Rotation: Repeat Mechanism")
+players = Player.make_n_players(3, names)
+rotate_strategies(game, players, rotation, 1000, mechanism=repeat_mechanism)
+print_players(players)
+
+rotation = [
+    [
+        strategy_home,
+        strategy_away,
+        strategy_home
+    ],
+    [
+        strategy_away,
+        strategy_away,
+        strategy_away,
+        strategy_away,
+        strategy_home,
+        strategy_away
+    ],
+    [
+        strategy_away,
+        strategy_home,
+        strategy_away,
+        strategy_away,
+        strategy_away,
+        strategy_away
+    ]
+]
+print("Six-Day Rotation: No Mechanism")
+players = Player.make_n_players(3, names)
+rotate_strategies(game, players, rotation, 1000)
+print_players(players)
+
+print("Six-Day Rotation: Repeat Mechanism")
+players = Player.make_n_players(3, names)
+rotate_strategies(game, players, rotation, 1000, mechanism=repeat_mechanism)
+print_players(players)
